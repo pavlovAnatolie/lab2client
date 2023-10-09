ddd32828c1582db2f229a1b309cfe63771d64feb
lai2-01 <lai2-01@itismeucci.com>
2023-10-09 09:49:21 +0200

    fin client

--- client/src/main/java/com/example/App.java
+++ client/src/main/java/com/example/App.java
@@ -1,13 +1,37 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    /**
+     * @param args
+     */
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            Socket s = new Socket("localhost", 3000);
+
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+            out.writeBytes("ciao mondo \n");
+            System.out.println(in.readLine());
+
+            
+            s.close();
+
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
     }
 }
